0636af089f070fe4bbdce73eb3ce109bed0486cc
z <yucezisan@gmail.com>
2026-04-17 18:16:22 +0300

    fix: registerDto

--- Perfume/Perfume/DTOs/RegisterDto.cs
+++ Perfume/Perfume/DTOs/RegisterDto.cs
@@ -5,20 +5,18 @@ namespace Perfume.DTOs
     public class RegisterDto
     {
 
-        public Guid Id { get; set; }
-
         [Required(ErrorMessage = "İsim alanı zorunludur.")]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Soyisim alanı zorunludur.")]
-        public string Surname { get; set; }
+        public string Surname { get; set; } = string.Empty ;
 
         [Required(ErrorMessage = "Mail alanı zorunludur.")]
         [EmailAddress(ErrorMessage = "Geçerli bir email adresi giriniz.")]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Şifre alanı zorunludur.")]
         [MinLength(6, ErrorMessage = "Şifre en az 6 karakter olmalıdır.")]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
     }
 }
