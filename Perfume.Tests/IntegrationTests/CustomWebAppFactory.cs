00c6871912ff0681fd7a99ea504031831b6f4983
z <yucezisan@gmail.com>
2026-04-22 16:34:40 +0300

    fix

--- Perfume.Tests/IntegrationTests/CustomWebAppFactory.cs
+++ Perfume.Tests/IntegrationTests/CustomWebAppFactory.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Perfume.Data; // Kendi DbContext'inin olduğu yer
+using Perfume.Data; 
 using System.Linq;
 using System.Data.Common;
 
@@ -14,21 +14,18 @@ namespace Perfume.Tests.IntegrationTests
         {
             builder.ConfigureServices(services =>
             {
-                // 1. NÜKLEER SEÇENEK: İçinde "DbContextOptions" geçen TÜM ayarları bul ve yok et!
                 var dbOptions = services.Where(d => d.ServiceType.Name.Contains("DbContextOptions")).ToList();
                 foreach (var option in dbOptions)
                 {
                     services.Remove(option);
                 }
 
-                // 2. Gizli kalan fiziksel SQL bağlantılarını da süpür
                 var dbConnections = services.Where(d => d.ServiceType == typeof(DbConnection)).ToList();
                 foreach (var conn in dbConnections)
                 {
                     services.Remove(conn);
                 }
 
-                // 3. Artık içerisi tertemiz! InMemory veritabanımızı gönül rahatlığıyla kurabiliriz.
                 services.AddDbContext<PerfumeDbContext>(options =>
                 {
                     options.UseInMemoryDatabase("InMemoryDbForTesting");
